0f43d56f7a95426a69639df52bcf125673210029
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-07-03 19:52:16 -0600

    tested and verified the cats as a service code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,46 @@
-//const fs = require('fs');
-const cmdInput = process.argv.slice(2);
-
 const needle = require('needle');
-needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${cmdInput[0]}`, (error, response, body) => {
-  //console.log(error);
-  if (error) {
-    throw new Error("Something went wrong with your request", error);
-  }
-  console.log(response.statusCode);
-  //console.log(response, response.statusCode);
-  console.log(body[0].description);
-});
+
+const fetchBreedDescription = (breed, callback) => {
+  needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response) => {
+    callback(error, response.body[0].description);
+  });
+};
+
+
+module.exports = fetchBreedDescription;
+
+// //const fs = require('fs');
+// const needle = require('needle');
+// //const cmdInput = process.argv.slice(2);
+
+// // needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${cmdInput[0]}`, (error, response, body) => {
+// //   //console.log(error);
+// //   if (error) {
+// //     throw new Error("Something went wrong with your request", error);
+// //   }
+// //   console.log(response.statusCode);
+// //   //console.log(response, response.statusCode);
+// //   console.log(body[0].description);
+// // });
+
+// const fetchBreedDescription = (breed, callback) => {
+//   needle.get(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response) => {
+//     // if (error) {
+//     //   throw new Error("Something went wrong with your request", error);
+//     // }
+
+//     callback(error, response.body[0].description);
+
+
+//     //console.log(response.statusCode);
+
+//     //console.log(response, response.statusCode);
+//     //callback(body[0].description);
+//   });
+// };
+
+// // const fetchBreedDescription ('Siberian', (error, description) => {
+
+// // });
+
+// module.exports = fetchBreedDescription;
